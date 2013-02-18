f70513b3c01567d01a3d77ae4f007ee36920bd7a
mambler <m.x.ambler@gmail.com>
2013-02-18 09:05:12 -0500

    Calculates the product of two numbers. No error checking.

---
+++ calc.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	double a,b;
+	
+	cout << "Input first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	cout << "Product = " << a*b << endl;	
+	
+	return 0;
+}
