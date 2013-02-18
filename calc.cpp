87f712f2c2ac8ba597e1219154c0edbc0e72f878
mambler <m.x.ambler@gmail.com>
2013-02-18 10:10:33 -0500

    Can now perform addition, division and subtraction too

--- calc.cpp
+++ calc.cpp
@@ -2,17 +2,53 @@
 #include<iostream>
 #include<math.h>
 
+//int error_check(double);
+
 using namespace std;
+
+void get_inputs(double&,double&);
 
 int main()
 {
-	double a,b;
+	double a,b,c;
+	char operation;
 	
-	cout << "Input first number: ";
-	cin >> a;
-	cout << "Enter second number: ";
-	cin >> b;
-	cout << "Product = " << a*b << endl;	
+	cout << "What operation do you wish to perform?\n\
+	'+' '-' '*' or '/'\n(Syntax is a.op.b=c)\n";
+	cin >> operation;
 	
+	// Check what operator has been entered
+	if(operation == '+'){
+		get_inputs(a,b);
+		c=a+b;
+	} else if(operation == '*') {
+		get_inputs(a,b);
+		c=a*b;
+	} else if(operation == '/') {
+		get_inputs(a,b);
+		c=a/b;
+	} else if(operation == '-') {
+		get_inputs(a,b);
+		c=a-b;
+	}
+	
+	cout << "c = " << c << endl;
+		
 	return 0;
 }
+
+void get_inputs(double&a,double&b)
+{
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	
+	return;
+}
+/*
+int error_check(double)
+{
+	if(!
+}
+*/
